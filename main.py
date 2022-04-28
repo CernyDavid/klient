0ddcb2f571a383a07c3e476e3e3d4809e7c29542
David Černý <91247706+CernyDavid@users.noreply.github.com>
2022-04-28 11:24:28 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 radio.set_group(80)
 serialnumber = control.device_serial_number()
-cislo = 64
+cislo = -1
 def on_button_pressed_b():
     global cislo
     cislo += 1
-    basic.show_string(String.from_char_code(cislo))
+    basic.show_string(String.from_char_code(cislo+65))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 def on_button_pressed_a():
     global cislo
     cislo -= 1
-    basic.show_string(String.from_char_code(cislo))
+    basic.show_string(String.from_char_code(cislo+65))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_ab():
     radio.send_value("hlas", cislo)
