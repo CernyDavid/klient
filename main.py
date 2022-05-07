a9f66c2ac3c401bd58c81045d195d78a6101e8f0
David Černý <91247706+CernyDavid@users.noreply.github.com>
2022-05-07 12:49:48 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 radio.set_group(80)
 sernumb = control.device_serial_number()
 cislo = -1
+allowed = True
+
+def on_received_value(povoleni, povoleno):
+    global allowed
+    if povoleno == 1:
+        allowed = True
+    if povoleno == 0:
+        allowed = False
+radio.on_received_value(on_received_value)
+
+if allowed:
+    basic.show_icon(IconNames.YES)
+if not allowed:
+    basic.show_icon(IconNames.NO)
+
 def on_button_pressed_b():
     global cislo
-    cislo += 1
-    basic.show_string(String.from_char_code(cislo+65))
+    if allowed:
+        basic.clear_screen()
+        cislo += 1
+        basic.show_string(String.from_char_code(cislo+65))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 def on_button_pressed_a():
     global cislo
-    cislo -= 1
-    basic.show_string(String.from_char_code(cislo+65))
+    if allowed:
+            basic.clear_screen()
+            cislo -= 1
+            basic.show_string(String.from_char_code(cislo+65))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_ab():
-    radio.send_value("hlas", cislo)
-    radio.send_value("serialnumber", sernumb)
+    if allowed:
+        radio.send_value("hlas", cislo)
+        radio.send_value("serialnumber", sernumb)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_received_value2(confirm, serial_confirm):
+    if serial_confirm == sernumb:
+        basic.show_icon(IconNames.HAPPY)
+radio.on_received_value(on_received_value2)
--- main.ts
+++ main.ts
@@ -1,17 +1,54 @@
 radio.setGroup(80)
 let sernumb = control.deviceSerialNumber()
 let cislo = -1
+let allowed = true
+radio.onReceivedValue(function on_received_value(povoleni: string, povoleno: number) {
+    
+    if (povoleno == 1) {
+        allowed = true
+    }
+    
+    if (povoleno == 0) {
+        allowed = false
+    }
+    
+})
+if (allowed) {
+    basic.showIcon(IconNames.Yes)
+}
+
+if (!allowed) {
+    basic.showIcon(IconNames.No)
+}
+
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    cislo += 1
-    basic.showString(String.fromCharCode(cislo + 65))
+    if (allowed) {
+        basic.clearScreen()
+        cislo += 1
+        basic.showString(String.fromCharCode(cislo + 65))
+    }
+    
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    cislo -= 1
-    basic.showString(String.fromCharCode(cislo + 65))
+    if (allowed) {
+        basic.clearScreen()
+        cislo -= 1
+        basic.showString(String.fromCharCode(cislo + 65))
+    }
+    
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    radio.sendValue("hlas", cislo)
-    radio.sendValue("serialnumber", sernumb)
+    if (allowed) {
+        radio.sendValue("hlas", cislo)
+        radio.sendValue("serialnumber", sernumb)
+    }
+    
+})
+radio.onReceivedValue(function on_received_value2(confirm: string, serial_confirm: number) {
+    if (serial_confirm == sernumb) {
+        basic.showIcon(IconNames.Happy)
+    }
+    
 })
